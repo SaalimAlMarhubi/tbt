66959469a9659d0f4f5df272c3afbfe424e602d9
saalimh <59394790+saalimh@users.noreply.github.com>
2022-09-28 20:58:26 +0100

    Responsive footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -51,18 +51,19 @@
 
   <footer class="footer">
     <div class="footer-content">
-      <div class="content">
+        <div class="footer-contact">
         <h2>Let's chat!</h2>
         <h4>
         <a href="tel:+441373469270">+44 (0)1373 469 270</a>
         </h4>
         <div>
-        <a href="https://twitter.com/tbt_marketing" class="social-link" target="_blank" rel="noopener"><img src="/images/twitter.svg" alt="Twitter"></a>
-        <a href="https://www.facebook.com/tbtmarketing" class="social-link" target="_blank" rel="noopener"><img src="/images/facebook.svg" alt="Facebook"></a>
-        <a href="https://www.linkedin.com/company/tbt-marketing" class="social-link" target="_blank" rel="noopener"><img src="/images/linkedin.svg" alt="LinkedIn"></a>
+        <a href="https://twitter.com/tbt_marketing" class="social-link" target="_blank" rel="noopener"><img src="https://www.tbtmarketing.com/images/twitter.svg" alt="Twitter"></a>
+        <a href="https://www.facebook.com/tbtmarketing" class="social-link" target="_blank" rel="noopener"><img src="https://www.tbtmarketing.com/images/facebook.svg" alt="Facebook"></a>
+        <a href="https://www.linkedin.com/company/tbt-marketing" class="social-link" target="_blank" rel="noopener"><img src="https://www.tbtmarketing.com/images/linkedin.svg" alt="LinkedIn"></a>
         </div>
+      </div>
         <br><br>
-        <div class="container container--grid footer-left">
+        <div class="">
         <div class="content">
         <h4>TBT Marketing</h4>
         <p>K1 &amp; K2, The Courtyard<br>Jenson Avenue<br>Commerce Park<br>Frome BA11 2FG, UK<br></p>
@@ -74,7 +75,6 @@
         <img src="https://api.thegreenwebfoundation.org/greencheckimage/www.tbtmarketing.com?nocache=true" alt="This website is hosted Green - checked by thegreenwebfoundation.org">
         </div>
         </div> --}}
-        </div>
         </div>
     </div>
   </footer>
